23a9781e1c896d5c0ba9b49297283eb2d95636a8
kwy404 <alexandre_rk_silva@estudante.sc.senai.br>
2022-05-24 13:32:42 -0300

    fix: any bugs with npm

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css';
+import Header from './components/Header';
+
+function App() {
+  
+  return (
+    <div className='theme-dark fullBody'>
+        <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,59 @@
+const Header = () => {
+    //Array with objects content linksMenu
+    const linksMenu = [
+        {
+            name: 'About',
+            path: 'about'
+        },
+        {
+            name: 'Experience',
+            path: 'jobs'
+        },
+        {
+            name: 'Work',
+            path: 'projects'
+        },
+        {
+            name: 'contact',
+            path: 'contact'
+        }
+    ];
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <div className="logo fade-enter-done" tabIndex={-1}/>
+                    <div className="menuLinkContent">
+                        <ol>
+                            { linksMenu.map(link => (
+                                <MenuLink 
+                                {...link}
+                                />
+                            ))}
+                        </ol>
+                        <div className="fadedown-enter-done" 
+                        style={{transitionDelay: '400ms'}}>
+                            <a className="resume-button" target="_blank" rel="noopener noreferrer">Resume</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+interface propsMenuLink {
+    name: string;
+    path?: string;
+}
+
+const MenuLink = ({name, path}: propsMenuLink) => {
+    return (
+        <li className="fadedown-enter-done" style={{transitionDelay: '0ms'}}>
+             <a href={`/#${path}`}>{name}</a>
+        </li>
+    )
+}
+
+export default Header;
