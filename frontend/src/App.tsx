1da17e0ad553f2dc2b67d85667cd6f393c6326c7
kwy404 <alexandre_rk_silva@estudante.sc.senai.br>
2022-05-24 12:48:47 -0300

    feat: add react-router-dom on app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Route, BrowserRouter } from "react-router-dom";
 import './App.css'
 
 function App() {
@@ -6,7 +7,9 @@ function App() {
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+           {/* <Route component = { Home }  path="/" exact /> */}
+      </BrowserRouter>
     </div>
   )
 }
