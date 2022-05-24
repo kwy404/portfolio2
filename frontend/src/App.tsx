40dcacf813acf262348c054492a058cd2313c507
kwy404 <alexandre_rk_silva@estudante.sc.senai.br>
2022-05-24 12:56:09 -0300

    fix: tainwind css import on app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { Route, BrowserRouter } from "react-router-dom";
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <BrowserRouter>
            {/* <Route component = { Home }  path="/" exact /> */}
       </BrowserRouter>
